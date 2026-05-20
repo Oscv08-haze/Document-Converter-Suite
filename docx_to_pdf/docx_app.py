a4759b823dffa901b5ade0d1e4d68fb524a94696
Oscv08-haze <oscarcardenas231008@gmail.com>
2026-05-19 19:35:13 -0600

    new converter 'docx_to_pdf'

---
+++ docx_to_pdf/docx_app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, send_file
+import os
+from docx_to_pdf.logic.engine_pdf import add_pdf
+from docx import Document
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def casa():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    archivo_docx = request.files['docx_file']
+
+    if archivo_docx and archivo_docx.filename.endswith('.docx'):
+        doc = Document(archivo_docx)
+        contenido = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+        nombre_base = os.path.splitext(archivo_docx.filename)[0]
+        nombre_pdf = f"{nombre_base}.pdf"
+        add_pdf(contenido, nombre_pdf)
+        return send_file(nombre_pdf, as_attachment=True)
+
+    return "Archivo no válido. Por favor, suba un archivo .docx.", 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
