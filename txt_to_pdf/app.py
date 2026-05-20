cd9e22b6b758fa0c9a7fc7ec8d97460819f3aa26
Oscv08-haze <oscarcardenas231008@gmail.com>
2026-05-19 19:00:02 -0600

    logic, static, templates and app.py are move to txt_to_pdf folder

--- txt_to_pdf/app.py
+++ txt_to_pdf/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, send_file
 import os
-from logic.motor_pdf import generar_pdf
+from txt_to_pdf.logic.motor_pdf import generar_pdf
 
 app = Flask(__name__)
 
